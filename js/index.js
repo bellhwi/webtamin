bf249d41081ab97617f1546083124cfd7eb6ed34
Jonghwi Kim <54308894+bellhwi@users.noreply.github.com>
2023-09-06 09:05:16 -0700

    Add columns

--- js/index.js
+++ js/index.js
@@ -1,28 +1,8 @@
-// Nav Toggle Button Control (Mobile)
-// Purpose: Hide and show mobile navigation menu
-//
-// 1. Assign the navigation toggle button to "navToggle" variable
-// 2. Assign the navigation content div to "navContent" variable
-// 3. "toggle" function to hide/show the navigation content
-// 4. Add click listener to navToggle to call toggle() on navContent when navToggle is clicked
+// JavaScript code to get and display the current year
+const currentYearElement = document.getElementById('currentYear')
 
-;(function buttonControlInit() {
-  const navOpen = document.querySelector('#nav_open')
-  const navClose = document.querySelector('#nav_close')
-  const navContent = document.querySelector('#nav_content')
+// Get the current year
+const currentYear = new Date().getFullYear()
 
-  // Toggles menu depending on id of event target ("nav_open" or "nav_close")
-  function toggleNav(e) {
-    if (e.target.id === 'nav_open') {
-      navContent.classList.add('right-0')
-      navContent.classList.remove('right-full')
-    } else {
-      navContent.classList.remove('right-0')
-      navContent.classList.add('right-full')
-    }
-  }
-
-  ;[navOpen, navClose].map((button) =>
-    button.addEventListener('click', toggleNav)
-  )
-})()
+// Set the current year in the HTML element
+currentYearElement.textContent = currentYear
