c57cbc6f323a5f00dce4ffc53210d22e59c0fe25
Jonghwi Kim <54308894+bellhwi@users.noreply.github.com>
2023-10-17 16:30:47 -0700

    update photos in columns

--- js/index.js
+++ js/index.js
@@ -1,8 +1,4 @@
-// JavaScript code to get and display the current year
+// Display the current year
 const currentYearElement = document.getElementById('currentYear')
-
-// Get the current year
 const currentYear = new Date().getFullYear()
-
-// Set the current year in the HTML element
 currentYearElement.textContent = currentYear
