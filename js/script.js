5121993f8a6b92ba1bd7fc63c5eef8301e0c1c58
Jonghwi Kim <54308894+bellhwi@users.noreply.github.com>
2024-06-12 22:33:48 -0700

    update conversion tags

--- js/script.js
+++ js/script.js
@@ -1,13 +1,49 @@
 const currentDate = new Date(),
   currentYear = document.getElementById('currentYear'),
   movingNav = document.getElementById('moving-nav')
-const emailSubscribeBtn = document.querySelector('.ml-form-embedSubmit button')
 
 let lastScrollTop = 0
 
-if (emailSubscribeBtn !== null) {
-  emailSubscribeBtn.onclick = function () {
-    return gtag_report_conversion_signup()
+// Select the node that will be observed for mutations
+var targetNode = document.body
+
+// Options for the observer (which mutations to observe)
+var config = { childList: true, subtree: true }
+
+// Callback function to execute when mutations are observed
+var callback = function (mutationsList, observer) {
+  for (let mutation of mutationsList) {
+    if (mutation.type === 'childList') {
+      var element = document.querySelector(
+        '.ml-embedded .ml-form-embedSubmit button'
+      )
+      if (element) {
+        // Element is now in the DOM, run your function
+        setSignupConversionTag()
+        // Once the element is found, you can stop observing
+        observer.disconnect()
+        break
+      }
+    }
+  }
+}
+
+// Create an observer instance linked to the callback function
+var observer = new MutationObserver(callback)
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config)
+
+function setSignupConversionTag() {
+  // Your code here
+  const emailSubscribeBtn = document.querySelector(
+    '.ml-embedded .ml-form-embedSubmit button'
+  )
+  console.log(emailSubscribeBtn)
+  if (emailSubscribeBtn !== null) {
+    emailSubscribeBtn.onclick = function () {
+      return gtag_report_conversion_signup()
+    }
   }
 }
 
